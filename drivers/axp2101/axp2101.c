006f516c727e5cf446024664f26b15b6a39fac2a
Noah Luskey <noah@vvvvvvvvvv.io>
2025-02-25 15:36:09 -0500

    🖲️ improve abstraction of axp2101 poweron gpio (#15)
    - this moves the handling of the axp2101 poweron interrupts into a gpio
    API-compatible device

--- drivers/axp2101/axp2101.c
+++ drivers/axp2101/axp2101.c
@@ -8,62 +8,23 @@
 // really is more like a GPIO expander with a single
 // GPIO input, and should be abstracted as a child
 // node on the axp2101 parent node. Not high priority.
-
 #define DT_DRV_COMPAT x_powers_axp2101
+
+#include "reg_axp2101.h"
 
 #include <errno.h>
 #include <stdbool.h>
 
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/i2c.h>
-#include <zephyr/drivers/gpio.h>
-#include <zephyr/sys/util.h>
+
 #include <zephyr/logging/log.h>
-#include <zephyr/input/input.h>
-
 LOG_MODULE_REGISTER(axp2101, CONFIG_AXP2101_LOG_LEVEL);
-
-// Chip ID value and register
-#define AXP2101_CHIP_ID 0x4A
-#define AXP2101_REG_CHIP_ID 0x03U
-
-// Register and bit to enable button battery charging (for RTC)
-#define AXP2101_CHARGER_FUEL_GAUGE_WATCHDOG_CTRL_REG 0x18U
-#define AXP2101_CHARGER_FUEL_GAUGE_WATCHDOG_CTRL_MASK_BUTTON_CHARGER BIT(2)
-
-// IRQ enable registers
-#define AXP2101_IRQ_ENABLE_0_REG 0x40U
-#define AXP2101_IRQ_ENABLE_1_REG 0x41U
-#define AXP2101_IRQ_ENABLE_1_MASK_PWRON_POSITIVE_EDGE BIT(0) // PWRON positive edge
-#define AXP2101_IRQ_ENABLE_1_MASK_PWRON_NEGATIVE_EDGE BIT(1) // PWRON negative edge
-#define AXP2101_IRQ_ENABLE_1_MASK_PWRON_LONG_PRESS BIT(2)    // PWRON long press
-#define AXP2101_IRQ_ENABLE_1_MASK_PWRON_SHORT_PRESS BIT(3)   // PWRON short press
-#define AXP2101_IRQ_ENABLE_2_REG 0x42U
-
-// IRQ status registers
-#define AXP2101_IRQ_STATUS_0_REG 0x48U
-#define AXP2101_IRQ_STATUS_1_REG 0x49U
-#define AXP2101_IRQ_STATUS_1_MASK_PWRON_POSITIVE_EDGE BIT(0) // PWRON positive edge
-#define AXP2101_IRQ_STATUS_1_MASK_PWRON_NEGATIVE_EDGE BIT(1) // PWRON negative edge
-#define AXP2101_IRQ_STATUS_1_MASK_PWRON_LONG_PRESS BIT(2)    // PWRON long press
-#define AXP2101_IRQ_STATUS_1_MASK_PWRON_SHORT_PRESS BIT(3)   // PWRON short press
-#define AXP2101_IRQ_STATUS_2_REG 0x4AU
 
 struct axp2101_config
 {
     struct i2c_dt_spec i2c;
     bool button_battery_charge_enable;
-    struct gpio_dt_spec int_gpio;
-    uint16_t short_press_code;
-    uint16_t long_press_code;
-};
-
-struct axp2101_data
-{
-    struct gpio_callback gpio_cb;
-    struct k_sem sem;
-    const struct axp2101_config *config;
-    const struct device *self;
 };
 
 #define CHECK_OK(ret)                  \
@@ -76,58 +37,16 @@ struct axp2101_data
         }                              \
     } while (0)
 
-static int axp2101_clear_interrupts(const struct i2c_dt_spec *i2c, uint8_t reg, uint8_t *value)
+static int axp2101_clear_interrupt_reg(const struct i2c_dt_spec *i2c, uint8_t reg, uint8_t *value)
 {
     CHECK_OK(i2c_reg_read_byte_dt(i2c, reg, value));
     CHECK_OK(i2c_reg_write_byte_dt(i2c, reg, *value));
     return 0;
 }
 
-static void axp2101_thread(void *d, void *, void *)
-{
-    struct axp2101_data *data = d;
-    const struct axp2101_config *config = data->config;
-    while (1)
-    {
-        k_sem_take(&data->sem, K_FOREVER);
-
-        // clear all interrupts, but we only care about the status 1 reg right now
-        uint8_t value = 0;
-        axp2101_clear_interrupts(&config->i2c, AXP2101_IRQ_STATUS_0_REG, &value);
-
-        value = 0;
-        axp2101_clear_interrupts(&config->i2c, AXP2101_IRQ_STATUS_1_REG, &value);
-
-        if ((value & AXP2101_IRQ_STATUS_1_MASK_PWRON_SHORT_PRESS) && (config->short_press_code != INPUT_KEY_RESERVED))
-        {
-            // short press - by convention, 0 is sent on release
-            input_report_key(data->self, config->short_press_code, 0, false, K_NO_WAIT);
-        }
-
-        if ((value & AXP2101_IRQ_STATUS_1_MASK_PWRON_LONG_PRESS) && (config->long_press_code != INPUT_KEY_RESERVED))
-        {
-            // long press - by convention, 0 is sent on release
-            input_report_key(data->self, config->long_press_code, 0, false, K_NO_WAIT);
-        }
-
-        axp2101_clear_interrupts(&config->i2c, AXP2101_IRQ_STATUS_2_REG, &value);
-    }
-}
-
-static void axp2101_interrupt_callback(const struct device *port,
-                                       struct gpio_callback *cb,
-                                       gpio_port_pins_t pins)
-{
-    struct axp2101_data *data = CONTAINER_OF(cb, struct axp2101_data, gpio_cb);
-    k_sem_give(&data->sem);
-}
-
 static int axp2101_init(const struct device *dev)
 {
     const struct axp2101_config *config = dev->config;
-    struct axp2101_data *data = dev->data;
-    data->self = dev;
-
     LOG_DBG("Initializing instance");
 
     if (!i2c_is_ready_dt(&config->i2c))
@@ -158,21 +77,12 @@ static int axp2101_init(const struct device *dev)
 
     // clear all pending interrupts
     uint8_t value;
-    CHECK_OK(axp2101_clear_interrupts(&config->i2c, AXP2101_IRQ_STATUS_0_REG, &value));
-    CHECK_OK(axp2101_clear_interrupts(&config->i2c, AXP2101_IRQ_STATUS_1_REG, &value));
-    CHECK_OK(axp2101_clear_interrupts(&config->i2c, AXP2101_IRQ_STATUS_2_REG, &value));
+    CHECK_OK(axp2101_clear_interrupt_reg(&config->i2c, AXP2101_IRQ_STATUS_0_REG, &value));
+    CHECK_OK(axp2101_clear_interrupt_reg(&config->i2c, AXP2101_IRQ_STATUS_1_REG, &value));
+    CHECK_OK(axp2101_clear_interrupt_reg(&config->i2c, AXP2101_IRQ_STATUS_2_REG, &value));
 
-    // configure button and register the interrupt callback
-    CHECK_OK(gpio_pin_configure_dt(&config->int_gpio, GPIO_INPUT | GPIO_ACTIVE_LOW));
-    CHECK_OK(gpio_pin_interrupt_configure_dt(&config->int_gpio, GPIO_INT_EDGE_TO_ACTIVE));
-    gpio_init_callback(&data->gpio_cb, axp2101_interrupt_callback, BIT(config->int_gpio.pin));
-    CHECK_OK(gpio_add_callback_dt(&config->int_gpio, &data->gpio_cb));
-
-    // and configure the interrupt register for short and long presses
-    const uint8_t mask =
-        AXP2101_IRQ_ENABLE_1_MASK_PWRON_LONG_PRESS |
-        AXP2101_IRQ_ENABLE_1_MASK_PWRON_SHORT_PRESS;
-    CHECK_OK(i2c_reg_update_byte_dt(&config->i2c, AXP2101_IRQ_ENABLE_1_REG, mask, mask));
+    // right now, the only thing that cares about the interrupts is the GPIO node,
+    // which will re-enable interrupts and service them with a thread as appropriate
 
     return 0;
 }
@@ -180,21 +90,11 @@ static int axp2101_init(const struct device *dev)
 // I2C must be initialized before the AXP2101 driver
 BUILD_ASSERT(CONFIG_AXP2101_INIT_PRIORITY > CONFIG_I2C_INIT_PRIORITY);
 
-#define AXP2101_DEFINE(inst)                                                                 \
-    static const struct axp2101_config config##inst = {                                      \
-        .i2c = I2C_DT_SPEC_INST_GET(inst),                                                   \
-        .button_battery_charge_enable = DT_INST_PROP(inst, button_battery_charge_enable),    \
-        .int_gpio = GPIO_DT_SPEC_INST_GET(inst, int_gpios),                                  \
-        .short_press_code = DT_INST_PROP_OR(inst, short_press_code, INPUT_KEY_RESERVED),     \
-        .long_press_code = DT_INST_PROP_OR(inst, long_press_code, INPUT_KEY_RESERVED)};      \
-    static struct axp2101_data data##inst = {                                                \
-        .sem = Z_SEM_INITIALIZER(data##inst.sem, 0, 1),                                      \
-        .config = &config##inst,                                                             \
-    };                                                                                       \
-    K_THREAD_DEFINE(axp2101_thread_##inst, 1024, axp2101_thread, &data##inst,                \
-                    NULL, NULL, K_PRIO_COOP(CONFIG_AXP2101_THREAD_PRIORITY), 0, 0);          \
-                                                                                             \
-    DEVICE_DT_INST_DEFINE(inst, axp2101_init, NULL, &data##inst, &config##inst, POST_KERNEL, \
+#define AXP2101_DEFINE(inst)                                                               \
+    static const struct axp2101_config config##inst = {                                    \
+        .i2c = I2C_DT_SPEC_INST_GET(inst),                                                 \
+        .button_battery_charge_enable = DT_INST_PROP(inst, button_battery_charge_enable)}; \
+    DEVICE_DT_INST_DEFINE(inst, axp2101_init, NULL, NULL, &config##inst, POST_KERNEL,      \
                           CONFIG_AXP2101_INIT_PRIORITY, NULL);
 
 DT_INST_FOREACH_STATUS_OKAY(AXP2101_DEFINE)
---
+++ drivers/axp2101/gpio_axp2101.c
@@ -0,0 +1,306 @@
+//
+// Copyright (c) 2025 Noah Luskey <noah@vvvvvvvvvv.io>
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#define DT_DRV_COMPAT x_powers_axp2101_gpio
+
+#include "reg_axp2101.h"
+#include <stdbool.h>
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/gpio/gpio_utils.h>
+#include <zephyr/drivers/i2c.h>
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(gpio_axp2101, CONFIG_AXP2101_LOG_LEVEL);
+
+#define CHECK_OK(ret)                  \
+    do                                 \
+    {                                  \
+        if (ret < 0)                   \
+        {                              \
+            LOG_ERR("Error: %d", ret); \
+            return ret;                \
+        }                              \
+    } while (0)
+
+struct gpio_axp2101_config
+{
+    // gpio_driver_config needs to be first
+    struct gpio_driver_config drv_cfg;
+    struct gpio_dt_spec int_gpio;
+    struct i2c_dt_spec i2c;
+};
+
+struct gpio_axp2101_data
+{
+    // gpio_driver_data needs to be first
+    struct gpio_driver_data common;
+
+    // for receiving the interrupt from the axp2101
+    struct gpio_callback gpio_cb;
+    struct k_sem sem;
+    const struct device *self;
+
+    // for generating new callbacks when the appropriate
+    // interrupt from the axp2101 occurs
+    enum gpio_int_mode int_mode;
+    enum gpio_int_trig int_trig;
+    sys_slist_t cb;
+
+    // state of the single GPIO
+    volatile bool raw;
+};
+
+static int gpio_axp2101_pin_configure(const struct device *dev, gpio_pin_t pin, gpio_flags_t flags)
+{
+    struct gpio_axp2101_data *data = dev->data;
+    if ((pin != 0) || (flags & GPIO_OUTPUT))
+    {
+        LOG_ERR("Bad arguments (pin: %d, flags: %d)", pin, flags);
+        return -ENOTSUP;
+    }
+
+    if (flags & GPIO_ACTIVE_LOW)
+    {
+        data->common.invert |= BIT(pin);
+    }
+    else
+    {
+        data->common.invert &= ~BIT(pin);
+    }
+
+    return 0;
+}
+
+static int gpio_axp2101_port_get_raw(const struct device *dev, gpio_port_value_t *value)
+{
+    struct gpio_axp2101_data *data = dev->data;
+    *value = data->raw ? BIT(0) : 0;
+    return 0;
+}
+
+static int gpio_axp2101_port_set_masked_raw(const struct device *dev, gpio_port_pins_t mask, gpio_port_value_t value)
+{
+    LOG_ERR("port_set_masked_raw not supported");
+    return -ENOTSUP;
+}
+
+static int gpio_axp2101_port_set_bits_raw(const struct device *dev, gpio_port_pins_t pins)
+{
+    LOG_ERR("port_set_bits_raw not supported");
+    return -ENOTSUP;
+}
+
+static int gpio_axp2101_port_clear_bits_raw(const struct device *dev, gpio_port_pins_t pins)
+{
+    LOG_ERR("port_clear_bits_raw not supported");
+    return -ENOTSUP;
+}
+
+static int gpio_axp2101_port_toggle_bits(const struct device *dev, gpio_port_pins_t pins)
+{
+    LOG_ERR("port_toggle_bits not supported");
+    return -ENOTSUP;
+}
+
+static int gpio_axp2101_pin_interrupt_configure(const struct device *dev, gpio_pin_t pin,
+                                                enum gpio_int_mode mode, enum gpio_int_trig trig)
+{
+    if (pin != 0)
+    {
+        LOG_ERR("Bad arguments (pin: %d)", pin);
+        return -ENOTSUP;
+    }
+
+    if ((mode != GPIO_INT_MODE_EDGE) && (mode != GPIO_INT_MODE_DISABLED))
+    {
+        // GPIO_INT_MODE_LEVEL not supported
+        LOG_ERR("Bad arguments (mode: %d)", mode);
+        return -ENOTSUP;
+    }
+
+    if ((trig != GPIO_INT_TRIG_BOTH) && (trig != GPIO_INT_TRIG_LOW) && (trig != GPIO_INT_TRIG_HIGH))
+    {
+        LOG_ERR("Bad arguments (trig: %d)", trig);
+        return -ENOTSUP;
+    }
+
+    struct gpio_axp2101_data *data = dev->data;
+    data->int_mode = mode;
+    data->int_trig = trig;
+
+    return 0;
+}
+
+static int gpio_axp2101_manage_callback(const struct device *dev, struct gpio_callback *callback, bool set)
+{
+    __ASSERT(callback, "No callback!");
+    __ASSERT(callback->handler, "No callback handler!");
+
+    struct gpio_axp2101_data *data = dev->data;
+    sys_slist_t *callbacks = &data->cb;
+
+    if (!sys_slist_is_empty(callbacks))
+    {
+        if (!sys_slist_find_and_remove(callbacks, &callback->node))
+        {
+            if (!set)
+            {
+                return -EINVAL;
+            }
+        }
+    }
+    else if (!set)
+    {
+        return -EINVAL;
+    }
+
+    if (set)
+    {
+        sys_slist_prepend(callbacks, &callback->node);
+    }
+
+    return 0;
+}
+
+static uint32_t gpio_axp2101_get_pending_int(const struct device *dev)
+{
+    LOG_ERR("get_pending_int not supported");
+    return -ENOTSUP;
+}
+
+static const struct gpio_driver_api gpio_axp2101_driver_api = {
+    .pin_configure = gpio_axp2101_pin_configure,
+    .port_get_raw = gpio_axp2101_port_get_raw,
+    .port_set_masked_raw = gpio_axp2101_port_set_masked_raw,
+    .port_set_bits_raw = gpio_axp2101_port_set_bits_raw,
+    .port_clear_bits_raw = gpio_axp2101_port_clear_bits_raw,
+    .port_toggle_bits = gpio_axp2101_port_toggle_bits,
+    .pin_interrupt_configure = gpio_axp2101_pin_interrupt_configure,
+    .manage_callback = gpio_axp2101_manage_callback,
+    .get_pending_int = gpio_axp2101_get_pending_int,
+};
+
+static void gpio_axp2101_thread(void *d, void *, void *)
+{
+    struct gpio_axp2101_data *data = d;
+    const struct device *dev = data->self;
+    const struct gpio_axp2101_config *config = dev->config;
+
+    while (1)
+    {
+        k_sem_take(&data->sem, K_FOREVER);
+
+        // we are only interested in the EDGE interrupt flags. Any other flags
+        // are not the responsibility of the GPIO node.
+        uint8_t irq_status;
+        int ret = i2c_reg_read_byte_dt(&config->i2c, AXP2101_IRQ_STATUS_1_REG, &irq_status);
+        if (ret < 0)
+        {
+            LOG_ERR("Could not read IRQ status: %d", ret);
+            continue;
+        }
+        if (irq_status & (AXP2101_IRQ_STATUS_1_MASK_PWRON_NEGATIVE_EDGE |
+                          AXP2101_IRQ_STATUS_1_MASK_PWRON_POSITIVE_EDGE))
+        {
+            LOG_PRINTK("GPIO interrupt\n");
+            if (irq_status & AXP2101_IRQ_STATUS_1_MASK_PWRON_NEGATIVE_EDGE)
+            {
+                LOG_PRINTK("Negative edge\n");
+                data->raw = false;
+            }
+            if (irq_status & AXP2101_IRQ_STATUS_1_MASK_PWRON_POSITIVE_EDGE)
+            {
+                LOG_PRINTK("Positive edge\n");
+                data->raw = true;
+            }
+        }
+
+        // write back to clear those flags
+        ret = i2c_reg_write_byte_dt(&config->i2c, AXP2101_IRQ_STATUS_1_REG, irq_status);
+        if (ret < 0)
+        {
+            LOG_ERR("Could not clear IRQ status: %d", ret);
+            continue;
+        }
+
+        // handle the callbacks as appropriate
+        bool should_fire = false;
+        if (data->int_mode == GPIO_INT_MODE_EDGE)
+        {
+            if (data->int_trig == GPIO_INT_TRIG_BOTH)
+            {
+                should_fire = true;
+            }
+            else if (data->int_trig == GPIO_INT_TRIG_LOW)
+            {
+                should_fire = !data->raw;
+            }
+            else if (data->int_trig == GPIO_INT_TRIG_HIGH)
+            {
+                should_fire = data->raw;
+            }
+        }
+
+        if (should_fire)
+        {
+            struct gpio_callback *cb, *tmp;
+            SYS_SLIST_FOR_EACH_CONTAINER_SAFE(&data->cb, cb, tmp, node)
+            {
+                if (cb->pin_mask & BIT(0))
+                {
+                    __ASSERT(cb->handler, "No callback handler!");
+                    cb->handler(dev, cb, cb->pin_mask & BIT(0));
+                }
+            }
+        }
+    }
+}
+
+static void gpio_axp2101_interrupt_callback(const struct device *port,
+                                            struct gpio_callback *cb,
+                                            gpio_port_pins_t pins)
+{
+    struct gpio_axp2101_data *data = CONTAINER_OF(cb, struct gpio_axp2101_data, gpio_cb);
+    k_sem_give(&data->sem);
+}
+
+static int gpio_axp2101_init(const struct device *dev)
+{
+    struct gpio_axp2101_data *data = dev->data;
+    const struct gpio_axp2101_config *config = dev->config;
+    data->self = dev;
+
+    CHECK_OK(gpio_pin_configure_dt(&config->int_gpio, GPIO_INPUT | GPIO_ACTIVE_LOW));
+    CHECK_OK(gpio_pin_interrupt_configure_dt(&config->int_gpio, GPIO_INT_EDGE_TO_ACTIVE));
+    gpio_init_callback(&data->gpio_cb, gpio_axp2101_interrupt_callback, BIT(config->int_gpio.pin));
+    CHECK_OK(gpio_add_callback(config->int_gpio.port, &data->gpio_cb));
+
+    // and configure the interrupt register for edge detection
+    const uint8_t mask =
+        AXP2101_IRQ_ENABLE_1_MASK_PWRON_NEGATIVE_EDGE |
+        AXP2101_IRQ_ENABLE_1_MASK_PWRON_POSITIVE_EDGE;
+    CHECK_OK(i2c_reg_update_byte_dt(&config->i2c, AXP2101_IRQ_ENABLE_1_REG, mask, mask));
+
+    return 0;
+}
+
+#define GPIO_AXP2101_DEFINE(inst)                                                        \
+    static const struct gpio_axp2101_config config##inst = {                             \
+        .drv_cfg = {                                                                     \
+            .port_pin_mask = GPIO_PORT_PIN_MASK_FROM_DT_INST(inst),                      \
+        },                                                                               \
+        .int_gpio = GPIO_DT_SPEC_GET(DT_INST_PARENT(inst), int_gpios),                   \
+        .i2c = I2C_DT_SPEC_GET(DT_INST_PARENT(inst)),                                    \
+    };                                                                                   \
+    static struct gpio_axp2101_data data##inst = {                                       \
+        .sem = Z_SEM_INITIALIZER(data##inst.sem, 0, 1),                                  \
+        .raw = DT_INST_PROP(inst, initial_state_high),                                   \
+    };                                                                                   \
+    K_THREAD_DEFINE(gpio_axp2101_thread_##inst, 1024, gpio_axp2101_thread, &data##inst,  \
+                    NULL, NULL, K_PRIO_COOP(CONFIG_GPIO_AXP2101_THREAD_PRIORITY), 0, 0); \
+    DEVICE_DT_INST_DEFINE(inst, gpio_axp2101_init, NULL, &data##inst, &config##inst,     \
+                          POST_KERNEL, CONFIG_GPIO_AXP2101_INIT_PRIORITY, &gpio_axp2101_driver_api);
+
+DT_INST_FOREACH_STATUS_OKAY(GPIO_AXP2101_DEFINE)
