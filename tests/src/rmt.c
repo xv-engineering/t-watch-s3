cde689daaa698ff0c8d28d74d54e51e9aba6941c
Noah Luskey <noah@vvvvvvvvvv.io>
2025-02-28 14:11:02 -0500

    modeling the individual channels as separate sub-nodes

--- tests/src/rmt.c
+++ tests/src/rmt.c
@@ -3,7 +3,7 @@
 
 ZTEST(rmt, test_rmt_tx)
 {
-    const struct device *rmt = DEVICE_DT_GET(DT_ALIAS(rmt_tx));
+    const struct device *rmt = DEVICE_DT_GET(DT_ALIAS(infrared));
     zassert_true(device_is_ready(rmt), "RMT device is not ready");
 
     int res = rmt_tx_set_carrier(rmt, true, K_MSEC(100), K_MSEC(100), RMT_TX_CARRIER_LEVEL_HIGH);
