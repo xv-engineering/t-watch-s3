ed958d95f3df9e6b6bc0b38ec5ad69b278bd3f5f
Noah Luskey <noah@vvvvvvvvvv.io>
2025-02-28 15:23:13 -0500

    channel being constructed now, no implementation yet

--- drivers/rmt_tx/rmt_tx_esp32.c
+++ drivers/rmt_tx/rmt_tx_esp32.c
@@ -1,38 +1,34 @@
+#include <zephyr/drivers/pinctrl.h>
 #include <zephyr/drivers/rmt_tx.h>
+#include <zephyr/init.h>
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(rmt_tx_esp32, CONFIG_RMT_TX_LOG_LEVEL);
 
 #define DT_DRV_COMPAT espressif_esp32_rmt_tx
 
 struct rmt_tx_esp32_config
 {
-    // intentionally empty
-};
-
-struct rmt_tx_esp32_data
-{
-    // intentionally empty
-};
-
-static int rmt_tx_esp32_set_carrier(const struct device *dev, bool carrier_en,
-                                    k_timeout_t high_duration, k_timeout_t low_duration,
-                                    rmt_tx_carrier_level_t carrier_level)
-{
-    return 0;
-}
-
-static struct rmt_tx_driver_api rmt_tx_esp32_driver_api = {
-    .set_carrier = rmt_tx_esp32_set_carrier,
+    const struct pinctrl_dev_config *pcfg;
 };
 
 static int rmt_tx_esp32_init(const struct device *dev)
 {
-    return 0;
+    const struct rmt_tx_esp32_config *config = dev->config;
+    int ret = pinctrl_apply_state(config->pcfg, PINCTRL_STATE_DEFAULT);
+    if (ret < 0)
+    {
+        LOG_ERR("Failed to configure RMT TX pins");
+        return ret;
+    }
+    return -0;
 }
 
-#define RMT_TX_ESP32_DEFINE(inst)                                                    \
-    static struct rmt_tx_esp32_data data##inst = {};                                 \
-    static const struct rmt_tx_esp32_config config##inst = {};                       \
-    DEVICE_DT_INST_DEFINE(inst, rmt_tx_esp32_init, NULL, &data##inst, &config##inst, \
-                          POST_KERNEL, CONFIG_RMT_TX_INIT_PRIORITY,                  \
-                          &rmt_tx_esp32_driver_api);
+#define RMT_TX_ESP32_DEFINE(inst)                                             \
+    PINCTRL_DT_DEFINE(DT_DRV_INST(inst));                                     \
+    static const struct rmt_tx_esp32_config config##inst = {                  \
+        .pcfg = PINCTRL_DT_DEV_CONFIG_GET(DT_DRV_INST(inst)),                 \
+    };                                                                        \
+    DEVICE_DT_INST_DEFINE(inst, rmt_tx_esp32_init, NULL, NULL, &config##inst, \
+                          POST_KERNEL, CONFIG_RMT_TX_INIT_PRIORITY, NULL);
 
 DT_INST_FOREACH_STATUS_OKAY(RMT_TX_ESP32_DEFINE)
---
+++ drivers/rmt_tx/rmt_tx_esp32_channel.c
@@ -0,0 +1,54 @@
+#include <zephyr/kernel.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/rmt_tx.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_DECLARE(rmt_tx_esp32, CONFIG_RMT_TX_LOG_LEVEL);
+
+#define DT_DRV_COMPAT espressif_esp32_rmt_tx
+
+struct rmt_tx_esp32_channel_config
+{
+    const struct device *parent;
+    uint8_t channel;
+};
+
+static int rmt_tx_esp32_set_carrier(const struct device *dev, bool carrier_en,
+                                    k_timeout_t high_duration, k_timeout_t low_duration,
+                                    rmt_tx_carrier_level_t carrier_level)
+{
+    return 0;
+}
+
+static struct rmt_tx_driver_api rmt_tx_esp32_driver_api = {
+    .set_carrier = rmt_tx_esp32_set_carrier,
+};
+
+static int rmt_tx_esp32_channel_init(const struct device *dev)
+{
+    const struct rmt_tx_esp32_channel_config *config = dev->config;
+    if (!device_is_ready(config->parent))
+    {
+        LOG_ERR("Parent device not ready");
+        return -ENODEV;
+    }
+    return 0;
+}
+
+// parent node must be initialized first
+BUILD_ASSERT(CONFIG_RMT_TX_CHANNEL_INIT_PRIORITY >= CONFIG_RMT_TX_INIT_PRIORITY);
+
+#define RMT_TX_ESP32_CHANNEL_DEFINE(node)                                        \
+    BUILD_ASSERT(DT_REG_ADDR(node) < 4);                                         \
+    static const struct rmt_tx_esp32_channel_config config##node = {             \
+        .parent = DEVICE_DT_GET(DT_PARENT(node)),                                \
+        .channel = DT_REG_ADDR(node),                                            \
+    };                                                                           \
+    DEVICE_DT_DEFINE(node, rmt_tx_esp32_channel_init, NULL, NULL, &config##node, \
+                     POST_KERNEL, CONFIG_RMT_TX_CHANNEL_INIT_PRIORITY,           \
+                     &rmt_tx_esp32_driver_api);
+
+#define RMT_TX_ESP32_CHANNEL_DEFINE_ALL(inst) \
+    DT_FOREACH_CHILD(DT_DRV_INST(inst), RMT_TX_ESP32_CHANNEL_DEFINE)
+
+DT_INST_FOREACH_STATUS_OKAY(RMT_TX_ESP32_CHANNEL_DEFINE_ALL)
