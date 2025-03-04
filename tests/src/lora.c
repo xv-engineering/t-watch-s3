39076f265a938359cd47b6d3991b5002bf94f94c
Noah Luskey <noah@vvvvvvvvvv.io>
2025-03-04 06:20:26 -0500

    lora send and receive tests working

--- tests/src/lora.c
+++ tests/src/lora.c
@@ -2,21 +2,26 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/lora.h>
 
-ZTEST(lora, test_lora_init)
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(lora, CONFIG_BRINGUP_LOG_LEVEL);
+
+static const struct lora_modem_config lora_base_config = {
+    .frequency = 915000000,
+    .bandwidth = BW_125_KHZ,
+    .datarate = SF_10,
+    .coding_rate = CR_4_8,
+    .preamble_len = 12,
+    .iq_inverted = false,
+    .tx = false,
+};
+
+ZTEST(lora, test_lora_send)
 {
     const struct device *lora = DEVICE_DT_GET(DT_ALIAS(lora));
     zassert_true(device_is_ready(lora), "Lora device not ready");
-    struct lora_modem_config config = {
-        .frequency = 915000000,
-        .bandwidth = BW_125_KHZ,
-        .datarate = SF_10,
-        .coding_rate = CR_4_8,
-        .preamble_len = 12,
-        .tx_power = 20,
-        .tx = true,
-        .iq_inverted = false,
-        .public_network = false,
-    };
+    struct lora_modem_config config = lora_base_config;
+    config.tx_power = 20;
+    config.tx = true;
 
     int ret = lora_config(lora, &config);
     zassert_equal(ret, 0, "Lora config failed (%d)", ret);
@@ -24,6 +29,30 @@ ZTEST(lora, test_lora_init)
     uint8_t data[] = {'H', 'e', 'l', 'l', 'o', ',', ' ', 'W', 'o', 'r', 'l', 'd', '!'};
     ret = lora_send(lora, data, sizeof(data));
     zassert_equal(ret, 0, "Lora send failed (%d)", ret);
+}
+
+ZTEST(lora, test_lora_receive)
+{
+    LOG_PRINTK("This test expects test_lora_send to be run on another device\n");
+    const struct device *lora = DEVICE_DT_GET(DT_ALIAS(lora));
+    zassert_true(device_is_ready(lora), "Lora device not ready");
+    struct lora_modem_config config = lora_base_config;
+
+    int ret = lora_config(lora, &config);
+    zassert_equal(ret, 0, "Lora config failed (%d)", ret);
+
+    uint8_t data[20];
+    int16_t rssi;
+    int8_t snr;
+    LOG_PRINTK("Waiting for data...\n");
+    const char *expected_data = "Hello, World!";
+    ret = lora_recv(lora, data, sizeof(data), K_SECONDS(10), &rssi, &snr);
+    zassert_equal(ret, strlen(expected_data), "Lora recv failed (%d)", ret);
+
+    LOG_INF("Received data: %.*s", ret, data);
+    LOG_INF("RSSI: %d, SNR: %d", rssi, snr);
+
+    zassert_mem_equal(data, expected_data, strlen(expected_data));
 }
 
 ZTEST_SUITE(lora, NULL, NULL, NULL, NULL, NULL);
