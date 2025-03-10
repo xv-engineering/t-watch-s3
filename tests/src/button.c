598e28699cc7bae858938dc2c96188ab8ee9a8e1
Noah Luskey <noah@vvvvvvvvvv.io>
2025-03-10 14:56:25 -0400

    clean up the tests and configuration

--- tests/src/button.c
+++ tests/src/button.c
@@ -5,7 +5,7 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_DECLARE(bringup, CONFIG_BRINGUP_LOG_LEVEL);
 
-struct test_data
+struct button_fixture
 {
     struct k_sem external_press_sem;
     struct k_sem external_release_sem;
@@ -14,93 +14,101 @@ struct test_data
     struct k_sem unexpected_sem;
 };
 
-static struct test_data button_events = {
-    .external_press_sem = Z_SEM_INITIALIZER(button_events.external_press_sem, 0, 2),
-    .external_release_sem = Z_SEM_INITIALIZER(button_events.external_release_sem, 0, 2),
-    .internal_press_sem = Z_SEM_INITIALIZER(button_events.internal_press_sem, 0, 2),
-    .internal_release_sem = Z_SEM_INITIALIZER(button_events.internal_release_sem, 0, 2),
-    .unexpected_sem = Z_SEM_INITIALIZER(button_events.unexpected_sem, 0, 1),
-};
-
 static void button_callback(struct input_event *evt, void *user_data)
 {
-    struct test_data *data = user_data;
+    struct button_fixture *f = user_data;
     if (evt->dev == DEVICE_DT_GET(DT_ALIAS(buttons)))
     {
         if (evt->code == INPUT_BTN_SIDE)
         {
             if (evt->value == 1)
             {
-                k_sem_give(&data->external_press_sem);
+                k_sem_give(&f->external_press_sem);
             }
             else
             {
-                k_sem_give(&data->external_release_sem);
+                k_sem_give(&f->external_release_sem);
             }
         }
         else if (evt->code == INPUT_BTN_EXTRA)
         {
             if (evt->value == 1)
             {
-                k_sem_give(&data->internal_press_sem);
+                k_sem_give(&f->internal_press_sem);
             }
             else
             {
-                k_sem_give(&data->internal_release_sem);
+                k_sem_give(&f->internal_release_sem);
             }
         }
     }
 }
 
-INPUT_CALLBACK_DEFINE(DEVICE_DT_GET(DT_ALIAS(buttons)), button_callback, &button_events);
-ZTEST(button, test_power_button)
+ZTEST_F(button, test_power_button)
 {
+    struct button_fixture *f = fixture;
     int ret;
     LOG_PRINTK("Short-Press the external button twice\n");
-    ret = k_sem_take(&button_events.external_press_sem, K_SECONDS(3));
+    ret = k_sem_take(&f->external_press_sem, K_SECONDS(3));
     zassert_equal(ret, 0);
     LOG_PRINTK("External button pressed\n");
-    ret = k_sem_take(&button_events.external_release_sem, K_SECONDS(3));
+    ret = k_sem_take(&f->external_release_sem, K_SECONDS(3));
     zassert_equal(ret, 0);
     LOG_PRINTK("External button released\n");
-    ret = k_sem_take(&button_events.external_press_sem, K_SECONDS(3));
+    ret = k_sem_take(&f->external_press_sem, K_SECONDS(3));
     zassert_equal(ret, 0);
     LOG_PRINTK("External button pressed again\n");
-    ret = k_sem_take(&button_events.external_release_sem, K_SECONDS(3));
+    ret = k_sem_take(&f->external_release_sem, K_SECONDS(3));
     zassert_equal(ret, 0);
     LOG_PRINTK("External button released again\n");
 
-    zassert_equal(k_sem_count_get(&button_events.unexpected_sem), 0, "Unexpected button event");
+    zassert_equal(k_sem_count_get(&f->unexpected_sem), 0, "Unexpected button event");
 }
 
-ZTEST(button, test_internal_button)
+ZTEST_F(button, test_internal_button)
 {
+    struct button_fixture *f = fixture;
     int ret;
     LOG_PRINTK("Press the internal button twice\n");
-    ret = k_sem_take(&button_events.internal_press_sem, K_SECONDS(3));
+    ret = k_sem_take(&f->internal_press_sem, K_SECONDS(3));
     zassert_equal(ret, 0); // a timeout indicates button wasn't pressed in time
     LOG_PRINTK("Internal button pressed\n");
-    ret = k_sem_take(&button_events.internal_release_sem, K_SECONDS(3));
+    ret = k_sem_take(&f->internal_release_sem, K_SECONDS(3));
     zassert_equal(ret, 0);
     LOG_PRINTK("Internal button released\n");
-    ret = k_sem_take(&button_events.internal_press_sem, K_SECONDS(3));
+    ret = k_sem_take(&f->internal_press_sem, K_SECONDS(3));
     zassert_equal(ret, 0);
     LOG_PRINTK("Internal button pressed again\n");
-    ret = k_sem_take(&button_events.internal_release_sem, K_SECONDS(3));
+    ret = k_sem_take(&f->internal_release_sem, K_SECONDS(3));
     zassert_equal(ret, 0);
     LOG_PRINTK("Internal button released again\n");
 
-    zassert_equal(k_sem_count_get(&button_events.unexpected_sem), 0, "Unexpected button event");
+    zassert_equal(k_sem_count_get(&f->unexpected_sem), 0, "Unexpected button event");
     ztest_test_pass();
 }
 
-static void button_tests_before(void *)
+static void *button_tests_setup(void)
 {
-    k_sem_reset(&button_events.external_press_sem);
-    k_sem_reset(&button_events.external_release_sem);
-    k_sem_reset(&button_events.internal_press_sem);
-    k_sem_reset(&button_events.internal_release_sem);
-    k_sem_reset(&button_events.unexpected_sem);
+    static struct button_fixture fixture = {
+        .external_press_sem = Z_SEM_INITIALIZER(fixture.external_press_sem, 0, 2),
+        .external_release_sem = Z_SEM_INITIALIZER(fixture.external_release_sem, 0, 2),
+        .internal_press_sem = Z_SEM_INITIALIZER(fixture.internal_press_sem, 0, 2),
+        .internal_release_sem = Z_SEM_INITIALIZER(fixture.internal_release_sem, 0, 2),
+        .unexpected_sem = Z_SEM_INITIALIZER(fixture.unexpected_sem, 0, 1),
+    };
+
+    INPUT_CALLBACK_DEFINE(DEVICE_DT_GET(DT_ALIAS(buttons)), button_callback, &fixture);
+    return &fixture;
 }
 
-ZTEST_SUITE(button, NULL, NULL, button_tests_before, NULL, NULL);
+static void button_tests_before(void *fixture)
+{
+    struct button_fixture *f = fixture;
+    k_sem_reset(&f->external_press_sem);
+    k_sem_reset(&f->external_release_sem);
+    k_sem_reset(&f->internal_press_sem);
+    k_sem_reset(&f->internal_release_sem);
+    k_sem_reset(&f->unexpected_sem);
+}
+
+ZTEST_SUITE(button, NULL, button_tests_setup, button_tests_before, NULL, NULL);
--- tests/src/touch.c
+++ tests/src/touch.c
@@ -1,6 +1,86 @@
 #include <zephyr/ztest.h>
+#include <zephyr/input/input.h>
+#include <zephyr/dt-bindings/input/input-event-codes.h>
 
-ZTEST(touch, test_touch)
+#include <zephyr/logging/log.h>
+LOG_MODULE_DECLARE(bringup, CONFIG_BRINGUP_LOG_LEVEL);
+
+struct touch_fixture
+{
+    bool test_in_progress;
+    struct k_sem touch_press_sem;
+    struct k_sem touch_drag_sem;
+    struct k_sem touch_release_sem;
+};
+
+static void touch_input_callback(struct input_event *evt, void *user_data)
+{
+    struct touch_fixture *f = user_data;
+    if (!f->test_in_progress)
+    {
+        // because it's impossible to un-register the callback
+        // but I don't want a ton of printing to the console on
+        // any touch event when not running a test.
+        return;
+    }
+
+    if (evt->dev == DEVICE_DT_GET(DT_ALIAS(touch)))
+    {
+        if (evt->type == INPUT_EV_ABS && (evt->code == INPUT_ABS_X || evt->code == INPUT_ABS_Y))
+        {
+            LOG_DBG("Touch drag event %d: %d\n", evt->code, evt->value);
+            k_sem_give(&f->touch_drag_sem);
+        }
+        else if (evt->type == INPUT_EV_KEY && evt->code == INPUT_BTN_TOUCH)
+        {
+            if (evt->value)
+            {
+                LOG_DBG("Touch press event\n");
+                k_sem_give(&f->touch_press_sem);
+            }
+            else
+            {
+                LOG_DBG("Touch release event\n");
+                k_sem_give(&f->touch_release_sem);
+            }
+        }
+    }
+}
+
+static void *touch_tests_setup(void)
+{
+    static struct touch_fixture fixture = {
+        // We expect at least 1 touch event (actually, sematically I really
+        // think it should be just 1 touch event, but driver reports an input
+        // event for every position report, so the test is written with that
+        // in mind)
+        .touch_press_sem = Z_SEM_INITIALIZER(fixture.touch_press_sem, 0, 1),
+        // doesn't necessarily need to match the expected number of drag events,
+        // but larger than 1 allows a rapid amount of events to sort of "buffer"
+        .touch_drag_sem = Z_SEM_INITIALIZER(fixture.touch_drag_sem, 0, 10),
+        // we expect 1 release event, so set this to 2 so we can catch too many
+        .touch_release_sem = Z_SEM_INITIALIZER(fixture.touch_release_sem, 0, 2),
+    };
+
+    INPUT_CALLBACK_DEFINE(DEVICE_DT_GET(DT_ALIAS(touch)), touch_input_callback, &fixture);
+    return &fixture;
+}
+
+static void touch_tests_before(void *fixture)
+{
+    struct touch_fixture *f = fixture;
+    k_sem_reset(&f->touch_press_sem);
+    k_sem_reset(&f->touch_release_sem);
+    f->test_in_progress = true;
+}
+
+static void touch_tests_after(void *fixture)
+{
+    struct touch_fixture *f = fixture;
+    f->test_in_progress = false;
+}
+
+ZTEST(touch, test_touch_init)
 {
     const struct device *touch = DEVICE_DT_GET(DT_ALIAS(touch));
     zassert_true(device_is_ready(touch), "Touch device is not ready");
@@ -8,4 +88,40 @@ ZTEST(touch, test_touch)
     ztest_test_pass();
 }
 
-ZTEST_SUITE(touch, NULL, NULL, NULL, NULL, NULL);
+ZTEST_F(touch, test_touch_press)
+{
+    struct touch_fixture *f = fixture;
+    int ret;
+    LOG_PRINTK("Scroll your finger across the screen\n");
+    // Expect at least 1 touch press event, 20 touch drag events, then only 1 touch release event.
+    //
+    // The reason it's "at least 1" touch press event is because the driver generates a press
+    // event for every position report.
+    //
+    // I actually think this is incorrect behavior, but that's a discussion for another time.
+    k_timeout_t timeout = K_SECONDS(3);
+    const k_timepoint_t end = sys_timepoint_calc(timeout);
+    size_t drag_event_count = 0;
+
+    ret = k_sem_take(&f->touch_press_sem, timeout);
+    zassert_equal(ret, 0, "Expected touch press event, got timeout");
+
+    do
+    {
+        timeout = sys_timepoint_timeout(end);
+        ret = k_sem_take(&f->touch_drag_sem, timeout);
+        zassert_equal(ret, 0, "Expected touch drag event, got timeout");
+        drag_event_count++;
+    } while (!K_TIMEOUT_EQ(timeout, K_NO_WAIT) && drag_event_count < 20);
+
+    ret = k_sem_take(&f->touch_release_sem, timeout);
+    zassert_equal(ret, 0, "Expected touch release event, got timeout");
+
+    // make sure there's no remaining release events. We expect only 1
+    zassert_equal(k_sem_count_get(&f->touch_release_sem), 0);
+
+    zassert_equal(drag_event_count, 20, "Expected 20 touch drag events, got %d", drag_event_count);
+    LOG_PRINTK("Touch released\n");
+}
+
+ZTEST_SUITE(touch, NULL, touch_tests_setup, touch_tests_before, touch_tests_after, NULL);
